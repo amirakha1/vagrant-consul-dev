e083c2ca49c8cd991540770f569c46cc69ea7a66
amirakha1 <47871437+amirakha1@users.noreply.github.com>
2019-04-15 17:41:34 +0000

    Revert "make it simple"

--- scripts/configure.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# enable and start service
-cp /vagrant/conf/consul.service /etc/systemd/system/consul.service
-systemctl enable consul.service
-systemctl start consul.service
